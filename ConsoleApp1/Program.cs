9650c022b9cf78bf0e74fd6ad3273b493328ba26
olegs <olegs@DESKTOP-T66HLTJ>
2023-09-27 22:09:58 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.ComponentModel.Design;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int port = 8080;
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    int bytes = 0;
+                    byte[] data = new byte[1024];
+                    bytes = listenSocket.ReceiveFrom(data, ref remoteEndPoint);
+
+                    string msg = Encoding.Unicode.GetString(data, 0, bytes);
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {msg} from {remoteEndPoint}");
+                    string message = "";
+                    if (msg.ToLower().Contains("hello"))
+                        message = "Hello!";
+                    else if (msg.ToLower().Contains("bye"))
+                        message = "Bye!";
+                    else
+                        message = "Message was send!";
+                    data = Encoding.Unicode.GetBytes(message);
+                    listenSocket.SendTo(data, remoteEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int port = int.Parse(PortTb.Text);
+            string address =IPTb.Text;
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                string message = MessageTb.Text;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.SendTo(data, ipPoint);
+                int bytes = 0;
+                string response = "";
+                data = new byte[1024]; // 1KB
+                do
+                {
+                    bytes = socket.ReceiveFrom(data, ref remoteIpPoint);
+                    response += Encoding.Unicode.GetString(data, 0, bytes);
+                } while (socket.Available > 0);
+
+                Dialog.Text += response + "\n";
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
